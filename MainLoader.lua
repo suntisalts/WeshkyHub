923a7d94c2cb6d187765b94ad3bd9eac76577452
suntisalts <suntisalts@gmail.com>
2025-04-06 21:00:44 +0200

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,11 @@
+local placeId = game.PlaceId
+
+if placeId == 537413528 then
+    print("Babft")
+elif placeId == 155615604 then
+    print("PrisonLife")
+else
+    print("Not Compatible")
+end
+
+-- HMMMMMMM WHAT THATS GONNA BE???
