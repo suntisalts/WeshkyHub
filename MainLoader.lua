dd98803e3956730ab0e65cb43c4e834fd7f2c399
suntisalts <suntisalts@gmail.com>
2025-04-06 21:02:02 +0200

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,11 +1,11 @@
 local placeId = game.PlaceId
 
 if placeId == 537413528 then
-    print("Babft")
+    task.wait(0.2)
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/suntisalts/WeshkyHub/refs/heads/main/Babft.lua'),true))()
 elif placeId == 155615604 then
-    print("PrisonLife")
+    task.wait(0.2)
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/WeshkyPL/WehskyHub/refs/heads/main/WeshkyHubV1.12.lua'),true))()
 else
     print("Not Compatible")
 end
-
--- HMMMMMMM WHAT THATS GONNA BE???
