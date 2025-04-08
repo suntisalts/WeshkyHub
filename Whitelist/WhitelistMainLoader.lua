f46345fd0172cdc64a7a7c4eb6f64153b930ffe5
suntisalts <suntisalts@gmail.com>
2025-04-08 17:33:56 +0200

    Update WhitelistMainLoader.lua

--- Whitelist/WhitelistMainLoader.lua
+++ Whitelist/WhitelistMainLoader.lua
@@ -1,10 +1,17 @@
 local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/suntisalts/WeshkyHub/refs/heads/main/Whitelist/Hwidlist.lua"))()
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
-for i,v in pairs(HWIDTable) do 
-	if v == HWID then
-		print ("The HWID is Whitelisted. HWID: ", HWID)
-	end elseif v ~= HWID then
-		print("The HWID is not Whitelisted. HWID: ", HWID)
-	end	
+local whitelisted = false
+
+for i, v in pairs(HWIDTable) do 
+    if v == HWID then
+        whitelisted = true
+        break
+    end
+end
+
+if whitelisted then
+    print("The HWID is Whitelisted. HWID: ", HWID)
+else
+    print("The HWID is not Whitelisted. HWID: ", HWID)
 end
