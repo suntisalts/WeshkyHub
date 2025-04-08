e4222f95b7a6fc1b5cb990eaf1c53a584876cdda
suntisalts <suntisalts@gmail.com>
2025-04-08 23:02:57 +0200

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -2,6 +2,7 @@ local placeId = game.PlaceId
 
 if placeId == 537413528 then
     error("Maybe it dosent look like it but @Minegoo is Gay.")
+    warn("@Minegoo is Gay.")
     task.wait(0.05)
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/suntisalts/WeshkyHub/refs/heads/main/Main/Babft.lua'),true))()
 elseif placeId == 155615604 then
