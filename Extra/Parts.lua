8e1d748d98a27def1fd3ca6283daf97a90f3df8a
suntisalts <suntisalts@gmail.com>
2025-05-11 05:42:49 +0200

    Create Parts.lua

---
+++ Extra/Parts.lua
@@ -0,0 +1,168 @@
+local Weshky1 = Instance.new("Part")
+Weshky1.Anchored = true
+Weshky1.Transparency = 1
+Weshky1.Size = Vector3.new(6, 1, 6)
+Weshky1.BottomSurface = Enum.SurfaceType.Smooth
+Weshky1.TopSurface = Enum.SurfaceType.Smooth
+Weshky1.Name = "Weshky1"
+Weshky1.Parent = game.Workspace
+
+local Weshky2 = Instance.new("Part")
+Weshky2.Anchored = true
+Weshky2.Transparency = 1
+Weshky2.Size = Vector3.new(6, 1, 6)
+Weshky2.Size = Vector3.new(6, 1, 6)
+Weshky2.BottomSurface = Enum.SurfaceType.Smooth
+Weshky2.TopSurface = Enum.SurfaceType.Smooth
+Weshky2.Name = "Weshky2"
+Weshky2.Parent = game.Workspace
+
+local Weshky3 = Instance.new("Part")
+Weshky3.Anchored = true
+Weshky3.Transparency = 1
+Weshky3.Size = Vector3.new(6, 1, 6)
+Weshky3.Size = Vector3.new(6, 1, 6)
+Weshky3.BottomSurface = Enum.SurfaceType.Smooth
+Weshky3.TopSurface = Enum.SurfaceType.Smooth
+Weshky3.Name = "Weshky3"
+Weshky3.Parent = game.Workspace
+
+local Weshky4 = Instance.new("Part")
+Weshky4.Anchored = true
+Weshky4.Transparency = 1
+Weshky4.Size = Vector3.new(6, 1, 6)
+Weshky4.Size = Vector3.new(6, 1, 6)
+Weshky4.BottomSurface = Enum.SurfaceType.Smooth
+Weshky4.TopSurface = Enum.SurfaceType.Smooth
+Weshky4.Name = "Weshky4"
+Weshky4.Parent = game.Workspace
+
+local Weshky5 = Instance.new("Part")
+Weshky5.Anchored = true
+Weshky5.Transparency = 1
+Weshky5.Size = Vector3.new(6, 1, 6)
+Weshky5.Size = Vector3.new(6, 1, 6)
+Weshky5.BottomSurface = Enum.SurfaceType.Smooth
+Weshky5.TopSurface = Enum.SurfaceType.Smooth
+Weshky5.Name = "Weshky5"
+Weshky5.Parent = game.Workspace
+
+local Weshky6 = Instance.new("Part")
+Weshky6.Anchored = true
+Weshky6.Transparency = 1
+Weshky6.Size = Vector3.new(6, 1, 6)
+Weshky6.Size = Vector3.new(6, 1, 6)
+Weshky6.BottomSurface = Enum.SurfaceType.Smooth
+Weshky6.TopSurface = Enum.SurfaceType.Smooth
+Weshky6.Name = "Weshky6"
+Weshky6.Parent = game.Workspace
+
+local Weshky7 = Instance.new("Part")
+Weshky7.Anchored = true
+Weshky7.Transparency = 1
+Weshky7.Size = Vector3.new(6, 1, 6)
+Weshky7.Size = Vector3.new(6, 1, 6)
+Weshky7.BottomSurface = Enum.SurfaceType.Smooth
+Weshky7.TopSurface = Enum.SurfaceType.Smooth
+Weshky7.Name = "Weshky7"
+Weshky7.Parent = game.Workspace
+
+local Weshky8 = Instance.new("Part")
+Weshky8.Anchored = true
+Weshky8.Transparency = 1
+Weshky8.Size = Vector3.new(6, 1, 6)
+Weshky8.Size = Vector3.new(6, 1, 6)
+Weshky8.BottomSurface = Enum.SurfaceType.Smooth
+Weshky8.TopSurface = Enum.SurfaceType.Smooth
+Weshky8.Name = "Weshky8"
+Weshky8.Parent = game.Workspace
+
+local Weshky9 = Instance.new("Part")
+Weshky9.Anchored = true
+Weshky9.Transparency = 1
+Weshky9.Size = Vector3.new(6, 1, 6)
+Weshky9.Size = Vector3.new(6, 1, 6)
+Weshky9.BottomSurface = Enum.SurfaceType.Smooth
+Weshky9.TopSurface = Enum.SurfaceType.Smooth
+Weshky9.Name = "Weshky9"
+Weshky9.Parent = game.Workspace
+
+local Weshky0 = Instance.new("Part")
+Weshky0.Anchored = true
+Weshky0.Transparency = 1
+Weshky0.Size = Vector3.new(6, 1, 6)
+Weshky0.Size = Vector3.new(6, 1, 6)
+Weshky0.BottomSurface = Enum.SurfaceType.Smooth
+Weshky0.TopSurface = Enum.SurfaceType.Smooth
+Weshky0.Name = "Weshky0"
+Weshky0.Parent = game.Workspace
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky1
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky2
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky3
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky4
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky5
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky6
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky7
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky8
+
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky9
+local Decal = Instance.new("Decal")
+Decal.Texture = "rbxassetid://137722555800117"
+Decal.Name = "Weshky Is Better Then Lexus!!"
+Decal.Face = Enum.NormalId.Top
+Decal.Parent = Weshky0
+
+Weshky1.Position = Vector3.new(-72.31890106201172,91.17544555664062,1191.0478515625)
+Weshky2.Position = Vector3.new(-52.304874420166016,58.03061294555664,1769.4705810546875)
+Weshky3.Position = Vector3.new(-27.725244522094727,67.33195495605469,2471.45361328125)
+Weshky4.Position = Vector3.new(-44.12211608886719,69.04801177978516,3165.256591796875)
+Weshky5.Position = Vector3.new(-53.77952194213867,74.39949798583984,3990.331298828125)
+Weshky6.Position = Vector3.new(-39.26005935668945,83.18901824951172,4824.65087890625)
+Weshky7.Position = Vector3.new(-52.22807312011719,67.77399444580078,5682.07421875)
+Weshky8.Position = Vector3.new(-82.22046661376953,50.52703094482422,6260.56982421875)
+Weshky9.Position = Vector3.new(-38.067955017089844,105.63947296142578,7167.95263671875)
+Weshky0.Position = Vector3.new(-49.93274688720703,57.78563690185547,7731.59375)
